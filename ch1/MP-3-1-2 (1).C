0eef50d5adaa6daadfe374415708b5db5138b358
Manpandya27 <pandyamann2704@gmail.com>
2023-05-19 16:52:40 +0530

    ch1

---
+++ ch1/MP-3-1-2 (1).C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("\t- - - - - \n");
+	printf("\t|        |\n");
+	printf("\tM        |\n");
+	printf("\tA        |\n");
+	printf("\tN        |\n");
+	printf("\tN        |\n");
+	printf("\t|        |\n");
+	printf("\t- - - - - \n");
+
+	getch();
+}
---
+++ ch1/MP1-3-1- (1).C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("NAME\t\t: Pandya Mann\n");
+	printf("Age\t\t: 16\n");
+	printf("Contact No\t: 9023*****7\n");
+	printf("Hobby\t\t: \"Coding\"\n");
+
+	getch();
+}
